50d34fbf70092236c4cb65a05b3e71fbf68931ff
cheppimdq <cheppimdp@gmail.com>
2023-12-16 14:57:23 -0300

    wp

--- js/app.js
+++ js/app.js
@@ -1,33 +1,34 @@
-const productosContainer = document.getElementById('productos-container'); // ID CONTENEDOR CON JS
+const productosContainer = document.getElementById('productos-container');
 const carritoLista = document.getElementById('carrito-lista');
 const carrito = [];
+const totalElement = document.getElementById('total');
+const confirmarPedidoBtn = document.getElementById('confirmarPedido');
+let totalCompra = 0;
 
 producto.forEach(producto => {
   const divProducto = document.createElement('div');
-  divProducto.classList.add('productos-css'); // CLASS CSS
+  divProducto.classList.add('productos-css');
 
-divProducto.innerHTML = `
-  <div>
-      <img class="foto-articulo" src="${producto.img || './ruta/por/defecto.jpg'}" alt="Foto ilustrativa del producto.">
-  </div>
-  <div>
-      <h3>${producto.nombre}</h3>
-      <h4>${producto.usos}</h4>
-      <p>Composición: ${producto.composicion}</p>
-      <p>Almacenamiento: ${producto.almacenamiento}</p>
-      <p>Precauciones: ${producto.precauciones}</p>
-      <p>Precio: $${producto.precio}</p>
-  </div>
-  <div class="botonera-articulo">
-    <button onclick="agregarAlCarrito('${producto.nombre}', ${producto.precio})">Agregar al carrito</button>
-    <button onclick="quitarDelCarrito('${producto.nombre}')">Quitar del carrito</button>
-  </div>
-`;
+  divProducto.innerHTML = `
+    <div>
+        <img class="foto-articulo" src="${producto.img || './ruta/por/defecto.jpg'}" alt="Foto ilustrativa del producto.">
+    </div>
+    <div>
+        <h3>${producto.nombre}</h3>
+        <h4>${producto.usos}</h4>
+        <p>Composición: ${producto.composicion}</p>
+        <p>Almacenamiento: ${producto.almacenamiento}</p>
+        <p>Precauciones: ${producto.precauciones}</p>
+        <p>Precio: $${producto.precio}</p>
+    </div>
+    <div class="botonera-articulo">
+      <button onclick="agregarAlCarrito('${producto.nombre}', ${producto.precio})">Agregar al carrito</button>
+      <button onclick="quitarDelCarrito('${producto.nombre}')">Quitar del carrito</button>
+    </div>
+  `;
   productosContainer.appendChild(divProducto);
 });
 
-
-// AGREGAR
 function agregarAlCarrito(nombre, precio) {
   const productoEnCarrito = carrito.find(item => item.nombre === nombre);
   if (productoEnCarrito) {
@@ -38,7 +39,6 @@ function agregarAlCarrito(nombre, precio) {
   actualizarCarrito();
 }
 
-// SACAR
 function quitarDelCarrito(nombre) {
   const productoEnCarritoIndex = carrito.findIndex(item => item.nombre === nombre);
   if (productoEnCarritoIndex !== -1) {
@@ -52,29 +52,37 @@ function quitarDelCarrito(nombre) {
   }
 }
 
-// ACTUALIZAR
 function actualizarCarrito() {
   carritoLista.innerHTML = '';
+  totalCompra = 0;
   carrito.forEach(producto => {
     const liCarrito = document.createElement('li');
     liCarrito.textContent = `${producto.nombre} - $${producto.precio} x${producto.cantidad}`;
     carritoLista.appendChild(liCarrito);
+    totalCompra += producto.precio * producto.cantidad;
   });
+  actualizarTotal();
 }
 
-// TOTAL
-const totalElement = document.getElementById('total');
-const confirmarPedidoBtn = document.getElementById('confirmarPedido');
-let totalCompra = 0;
 function actualizarTotal() {
   totalElement.textContent = totalCompra.toFixed(2);
 }
 
-// CONFIRMAR
 function confirmarPedido() {
-  const resumenPedido = `Broders les encargo \n\nTotal: $${totalCompra.toFixed(2)}`;
+  let resumenPedido = 'Broders, les encargo los siguientes productos:\n\n';
+
+  carrito.forEach(producto => {
+    resumenPedido += `${producto.nombre} - $${producto.precio} x${producto.cantidad}\n`;
+  });
+
+  resumenPedido += `\nTotal de productos: $${totalCompra.toFixed(2)}`;
+
   const mensajeWhatsApp = encodeURIComponent(resumenPedido);
   const numeroWhatsApp = '5492215718347';
   const enlaceWhatsApp = `https://api.whatsapp.com/send?phone=${numeroWhatsApp}&text=${mensajeWhatsApp}`;
   window.open(enlaceWhatsApp, '_blank');
 }
+
+
+// Llama a actualizarCarrito después de inicializar el carrito con productos
+actualizarCarrito();
